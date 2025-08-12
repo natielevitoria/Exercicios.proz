59472a98e5ea2be7a83baf08a07de4437b32a938
Natiele Cruz <145495586+natielevitoria@users.noreply.github.com>
2025-08-12 08:26:37 -0300

    Update index.js

--- Java script/Aula 1 - 08/index.js
+++ Java script/Aula 1 - 08/index.js
@@ -1,4 +1,5 @@
-function mudarMensagem(){ // Função que muda a mensagem quando o botão é clicado
-  const mensagem=document.getElementById("mensagem"); // Seleciona o elemento com o id "mensagem"
-  mensagem.innerHTML=" Parabéns, você acaba de ganhar 5% de desconto em todos os produtos. <br> Aproveite!"; // Muda o conteúdo HTML, ou seja, a mensagem exibida
+function mudarMensagem(){ 
+  const mensagem=document.getElementById("mensagem"); 
+  mensagem.innerHTML=" Parabéns, você acaba de ganhar 5% de desconto em todos os produtos. <br> Aproveite!";
+
 }
