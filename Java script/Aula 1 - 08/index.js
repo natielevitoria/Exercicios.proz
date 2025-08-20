57be934e85f02f6c902dcbaaf6d892ca21b5de45
Natiele Cruz <145495586+natielevitoria@users.noreply.github.com>
2025-08-19 21:30:55 -0300

    Delete Java script/Aula 1 - 08 directory

--- Java script/Aula 1 - 08/index.js
+++
@@ -1,5 +0,0 @@
-function mudarMensagem(){ 
-  const mensagem=document.getElementById("mensagem"); 
-  mensagem.innerHTML=" Parabéns, você acaba de ganhar 5% de desconto em todos os produtos. <br> Aproveite!";
-
-}
