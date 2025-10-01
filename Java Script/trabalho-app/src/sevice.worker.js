a47ae59e6146fdc4d15ae885e759ce3f5a64ba5c
Natiele Cruz <145495586+natielevitoria@users.noreply.github.com>
2025-10-01 08:59:41 -0300

    att

---
+++ Java Script/trabalho-app/src/sevice.worker.js
@@ -0,0 +1,22 @@
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open("app-saudacao-cache").then(cache => {
+            return cache.addAll([
+                "index.html",
+                "manifest.json",
+                "icone-192.png",
+                "icon-512.png",
+                "img/manha.jpg",
+                "img/tarde.png",
+                "img/noite.jpg",
+                "img/madrugada.jpg"
+            ]);
+        })
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(response => response || fetch(event.request))
+    );
+});
